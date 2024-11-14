65a992b04615d68a47968718cf3ee914627003a9
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2024-11-14 13:50:39 +0200

    Even Or Odd

--- Algorithms & problem solving level 1.cpp
+++ Algorithms & problem solving level 1.cpp
@@ -3,24 +3,58 @@
 
 using namespace std;
 
-void PrintName(string Name)
+//void PrintName(string Name)
+//{
+//    cout << "\n Your Name Is: " << Name << endl;
+//}
+//
+//string ReadName()
+//{
+//    string Name;
+//    cout << "Your Name? \n";
+//    getline(cin, Name);
+//    return Name;
+//}
+
+enum EnNumberType{Odd = 1 , Even = 2};
+
+int ReadNumber()
 {
-    cout << "\n Your Name Is: " << Name << endl;
+    int Num;
+
+    cout << "Enter Your Number ?\n";
+    cin >> Num;
+    return Num;
 }
 
-string ReadName()
+EnNumberType CheckNumberType(int Num)
 {
-    string Name;
-    cout << "Your Name? \n";
-    getline(cin, Name);
-    return Name;
+    int Result = Num % 2;
+    if (Result == 0)
+    {
+        return EnNumberType::Odd;
+    }
+    else {
+        return EnNumberType::Even;
+    }
+}
+
+void PrintNumType(EnNumberType NumberType)
+{
+    if (NumberType == EnNumberType::Even)
+    {
+        cout << "\n Your Number is Even. \n";
+    }
+    else {
+        cout << "\n Your Number is Odd. \n";
+    }
 }
 
 int main()
 {
     //PrintName("Ahmed Shaaban Al-Saidi");
+    //PrintName(ReadName());
 
-    PrintName(ReadName());
-
+    PrintNumType(CheckNumberType(ReadNumber()));
     return 0;
 }
