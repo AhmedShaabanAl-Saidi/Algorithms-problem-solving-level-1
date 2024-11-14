2028743cb0ffb76b6020fb47c2ec8f09b951fea9
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2024-11-14 12:53:56 +0200

    Add project files.

---
+++ Algorithms & problem solving level 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void PrintName(string Name)
+{
+    cout << "\n Your Name Is: " << Name << endl;
+}
+
+int main()
+{
+    // 1-Print Your Name
+    PrintName("Ahmed Shaaban Al-Saidi");
+
+    return 0;
+}
